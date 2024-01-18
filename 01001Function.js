f7de102ad29fd589c0a5822e1da99047558dac15
Topp Tobias <Tobias.Topp@coop.ch>
2024-01-18 13:14:15 +0100

    Initial commit

---
+++ 01001Function.js
@@ -0,0 +1,25 @@
+let output = ''; // Initialisiere den Output-String
+let count = 0; // Initialisiere den Zähler
+
+function generateRandomBinary() {
+  const randomNumber = Math.random(); // Generiere eine Zufallszahl zwischen 0 und 1
+  const binaryValue = Math.round(randomNumber);
+
+  const binaryParagraph = document.getElementById("BinaryParagraph");
+
+  // Füge die neue Zahl zum Paragraphen hinzu
+  output += binaryValue;
+
+  count++;
+
+  // Füge einen Zeilenumbruch nach jeder 83. Zahl hinzu
+  
+
+  // Aktualisiere den Inhalt des Paragraphen
+  binaryParagraph.innerHTML = output;
+
+  const randomInterval = Math.random() * 3 + 1; // Generiere eine Zufallszahl zwischen 5 und 155 Millisekunden
+  setTimeout(generateRandomBinary, randomInterval); // Setze die nächste Ausführung der Funktion mit dem zufälligen Intervall
+}
+
+generateRandomBinary();
